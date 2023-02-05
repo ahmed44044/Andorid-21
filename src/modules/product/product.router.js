0cb798c20656af07646c88030b6bc1b4429571fe
ahmed44044 <105466981+ahmed44044@users.noreply.github.com>
2023-02-06 00:45:23 +0200

    first commit

---
+++ src/modules/product/product.router.js
@@ -0,0 +1,17 @@
+
+import { Router } from "express";
+import * as UpdateController from './controller/update.js'
+import productModel from "../../../DB/model/product.model.js";
+import cloudinary from "../../services/cloudinary.js";
+import {  fileValidation, myMulter } from "../../services/multer.js";
+const router= Router()
+
+
+
+
+router.post('/',myMulter(fileValidation.image).array('images',5),UpdateController.addData)
+
+router.get('/:placeName',UpdateController.showData)
+
+
+export default router
